f2b285ed54ad5547d5fdf8815ce6201ed5adf5b7
sociosofia <luizvjj@gmail.com>
2026-09-15 15:53:25 -0300

    Corrigir aninhamento de formatação richtext

--- js/richtext.js
+++ js/richtext.js
@@ -3,6 +3,11 @@
 
 const SELECTOR = '#questionStatement, #supportContent, .alt-text';
 const cache = new WeakMap();
+const TOKENS = {
+  strongOpen: '\uE000', strongClose: '\uE001',
+  emOpen: '\uE002', emClose: '\uE003',
+  underlineOpen: '\uE004', underlineClose: '\uE005'
+};
 
 function escapeHtml(value) {
   return String(value ?? '').replace(/[&<>"']/g, char => ({
@@ -14,23 +19,37 @@ function escapeHtml(value) {
   })[char]);
 }
 
-function normalizeAllowedTags(value) {
+function protectAllowedTags(value) {
   return String(value ?? '')
     .replace(/<\s*br\s*\/?>/gi, '\n')
-    .replace(/<\s*\/\s*(strong|b)\s*>/gi, '**')
-    .replace(/<\s*(strong|b)\s*>/gi, '**')
-    .replace(/<\s*\/\s*(em|i)\s*>/gi, '*')
-    .replace(/<\s*(em|i)\s*>/gi, '*')
-    .replace(/<\s*\/\s*u\s*>/gi, '++')
-    .replace(/<\s*u\s*>/gi, '++');
+    .replace(/<\s*(strong|b)\s*>/gi, TOKENS.strongOpen)
+    .replace(/<\s*\/\s*(strong|b)\s*>/gi, TOKENS.strongClose)
+    .replace(/<\s*(em|i)\s*>/gi, TOKENS.emOpen)
+    .replace(/<\s*\/\s*(em|i)\s*>/gi, TOKENS.emClose)
+    .replace(/<\s*u\s*>/gi, TOKENS.underlineOpen)
+    .replace(/<\s*\/\s*u\s*>/gi, TOKENS.underlineClose);
+}
+
+function restoreAllowedTags(value) {
+  return value
+    .replaceAll(TOKENS.strongOpen, '<strong>')
+    .replaceAll(TOKENS.strongClose, '</strong>')
+    .replaceAll(TOKENS.emOpen, '<em>')
+    .replaceAll(TOKENS.emClose, '</em>')
+    .replaceAll(TOKENS.underlineOpen, '<u>')
+    .replaceAll(TOKENS.underlineClose, '</u>');
 }
 
 function toSafeHtml(value) {
-  let text = normalizeAllowedTags(value).replace(/\r\n?/g, '\n');
+  let text = protectAllowedTags(value).replace(/\r\n?/g, '\n');
   text = escapeHtml(text);
+
+  text = text.replace(/\*\*\*([^*\n]+?)\*\*\*/g, '<strong><em>$1</em></strong>');
   text = text.replace(/\*\*([\s\S]+?)\*\*/g, '<strong>$1</strong>');
   text = text.replace(/\+\+([\s\S]+?)\+\+/g, '<u>$1</u>');
   text = text.replace(/(^|[^*])\*([^*\n]+?)\*(?!\*)/g, '$1<em>$2</em>');
+
+  text = restoreAllowedTags(text);
   return text.replace(/\n/g, '<br>');
 }
 
@@ -53,6 +72,12 @@ function scan(root = document) {
   }
   root.querySelectorAll?.(SELECTOR).forEach(renderElement);
 }
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { toSafeHtml };
+}
+
+if (typeof window === 'undefined' || typeof document === 'undefined') return;
 
 function init() {
   scan(document);
